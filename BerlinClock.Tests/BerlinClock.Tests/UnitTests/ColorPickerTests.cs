3f69f63df571edf45ee00753cbeb17a5a89534c5
Artur Gorbaczewski <artur.gorbaczewski@outlook.com>
2020-04-16 16:32:32 +0200

    Initial commit

---
+++ BerlinClock.Tests/BerlinClock.Tests/UnitTests/ColorPickerTests.cs
@@ -0,0 +1,37 @@
+﻿using BerlinClock.ClockDomain.DomainFacade;
+using BerlinClock.Consts;
+using BerlinClock.Enums;
+using NUnit.Framework;
+
+namespace BerlinClock.Tests.UnitTests
+{
+    [TestFixture]
+    public class ColorPickerTests
+    {
+        [Test]
+        public void PickLightColorPicksYellowForTopLight()
+        {
+            // Arrange
+            var _testee = new ColorPicker();
+
+            // Act
+            var color = _testee.PickLightColor(1, RowType.TopLightLow);
+
+            // Assert
+            Assert.AreEqual(LightColor.Yellow, color);
+        }
+
+        [Test]
+        public void PickLightColorPicksRedForTopMinuteRowOnThirdLight()
+        {
+            // Arrange
+            var _testee = new ColorPicker();
+
+            // Act
+            var color = _testee.PickLightColor(3, RowType.TopMinuteRow);
+
+            // Assert
+            Assert.AreEqual(LightColor.Red, color);
+        }
+    }
+}
